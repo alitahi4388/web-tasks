44b9f2be13654d47d5d43b8d9cba3cb39de7e406
alitahi4388 <163639372+alitahi4388@users.noreply.github.com>
2024-05-26 07:13:39 -0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,12 @@
+var name = "Ali";
+var name = "Tayyab";
+var name = "Jawad";
+
+let age=10;
+const salary=100;
+age=61;
+
+console.log(salary);
+var num1=10;
+var num2=2.5;
+console.log(name + num1 + num2);
